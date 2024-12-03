8f9afd5a7a7b547af2c256162a3774543a378a44
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-03 12:55:03 +0700

    create a controller method to create user and its validation

--- src/items/item.entity.ts
+++ src/items/item.entity.ts
@@ -3,14 +3,19 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 export class Item {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   name: string;
+
   @Column()
   description: string;
+
   @Column()
   price: number;
+
   @Column()
   location: string;
+
   @Column()
   category: string;
 }
