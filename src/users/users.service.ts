8ade786ba8a3bf7307895fe792143e13ed9cb69d
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-03 15:20:41 +0700

    method untuk mendapatkan data user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,8 +9,24 @@ export class UsersService {
     @InjectRepository(User) private readonly userRepo: Repository<User>,
   ) {}
 
+  find() {
+    return this.userRepo.find();
+  }
+
   create(name: string, email: string, password: string) {
     const user = this.userRepo.create({ name, email, password });
     return this.userRepo.save(user);
   }
+
+  findOneBy(id: number) {
+    return this.userRepo.findOneBy({ id });
+  }
+
+  // update(id: number, attrs: Partial<User>) {
+  //   return this.userRepo.update(id, attrs);
+  // }
+
+  // remove(id: number) {
+  //   return this.userRepo.delete(id);
+  // }
 }
