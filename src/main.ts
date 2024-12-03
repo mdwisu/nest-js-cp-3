db6ecadd19dfcd89d04f1f24d451b27814b960fd
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-03 21:27:13 +0700

    setup session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+import cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({ keys: ['asdf'] }));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
