471da044f3a6500340b78679c89f5e4e4c4ef930
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-03 20:01:44 +0700

    fungsi registrasi

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+
+  async register(name: string, email: string, password: string) {
+    // check apakaah ada user yang menggunakan email sama
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('Email sudah terdaftar');
+    }
+    // hash
+    const hashedPassword = password;
+    // create user
+  }
 }
