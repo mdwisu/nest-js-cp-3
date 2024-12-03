dba9cf57e9fedd7b781d87b1c8346d192ac0355f
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-03 17:33:21 +0700

    mengurai data res melalui dto pada interceptor

--- src/interceptors/serialize.interceptors.ts
+++ src/interceptors/serialize.interceptors.ts
@@ -1,17 +1,17 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { UserDto } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
-    // run something before a request is handled
-    console.log('I am running before the handler', context);
     return next.handle().pipe(
       map((data: any) => {
-        console.log('I am running before the response is sent out', data);
+        return plainToClass(UserDto, data, { excludeExtraneousValues: true });
       }),
     );
   }
