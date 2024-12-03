eaae77bcc57133fab3479b54de0788073bbf9a50
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-03 20:21:46 +0700

    menyimpan data user dengan pass sudah di hashed

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -15,10 +15,15 @@ export class AuthService {
     if (users.length) {
       throw new BadRequestException('Email sudah terdaftar');
     }
+
     // hash
     const salt = randomBytes(8).toString('hex');
     const hash = (await scrypt(password, salt, 64)) as Buffer;
-    const result = salt + '.' + hash.toString('hex');
+    const hashedPassword = salt + '.' + hash.toString('hex');
+
     // create user
+    const user = await this.usersService.create(name, email, hashedPassword);
+
+    return user;
   }
 }
