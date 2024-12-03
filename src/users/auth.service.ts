9f5aceb7e7d9d9589269b650e7cfb2c01bfa161a
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-03 20:33:59 +0700

    membuat service proses login

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
@@ -23,6 +27,22 @@ export class AuthService {
 
     // create user
     const user = await this.usersService.create(name, email, hashedPassword);
+
+    return user;
+  }
+
+  async login(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+    if (!user) {
+      throw new NotFoundException('User tidak ditemukan');
+    }
+
+    const [salt, storedHash] = user.password.split('.');
+    const hash = (await scrypt(password, salt, 64)) as Buffer;
+
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('Password salah');
+    }
 
     return user;
   }
