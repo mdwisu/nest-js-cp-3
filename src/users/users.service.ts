4773a36bcaaf1445819dbc30363bce29f2b49f02
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-03 13:11:30 +0700

    proses menyimpan data uesr melalui service dan repository

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly userRepo: Repository<User>,
+  ) {}
+
+  create(name: string, email: string, password: string) {
+    const user = this.userRepo.create({ name, email, password });
+    return this.userRepo.save(user);
+  }
+}
