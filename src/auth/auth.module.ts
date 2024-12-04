0a72803be049b2e1a1645c8c9a5c9cae3486e470
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-04 21:51:36 +0700

    mengenal intergration testing atau e2e

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersService } from 'src/users/users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/users/user.entity';
-import { CurrentUserInterceptor } from 'src/auth/interceptors/current-user.interceptor';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { UsersService } from '../users/users.service';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { User } from '../users/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
