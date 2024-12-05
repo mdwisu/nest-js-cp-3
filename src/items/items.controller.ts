938cc4f902155f66a36d82bd49f6df521e0053d3
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-05 13:39:37 +0700

    mengubah response create item dengan class transform

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -4,12 +4,15 @@ import { ItemsService } from './items.service';
 import { AuthGuard } from '../guards/auth.guard';
 import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import { User } from '../users/user.entity';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { ItemDto } from './dtos/item.dto';
 
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
   @Post('/')
+  @Serialize(ItemDto)
   @UseGuards(AuthGuard)
   createItem(@Body() body: CreateItemDto, @CurrentUser() user: User) {
     return this.itemsService.create(body, user);
