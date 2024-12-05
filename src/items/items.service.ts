1c9c6375126e27148945bd95f91db82c1a8e3467
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-05 12:11:02 +0700

    membuat service create item

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Item } from './item.entity';
+import { Repository } from 'typeorm';
+import { CreateItemDto } from './dtos/create-item.dto';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  constructor(
+    @InjectRepository(Item) private readonly itemRepo: Repository<Item>,
+  ) {}
+
+  create(item: CreateItemDto) {
+    const newItem = this.itemRepo.create(item);
+    return this.itemRepo.save(newItem);
+  }
+}
